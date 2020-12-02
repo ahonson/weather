d38edaa284fa6dd70362cd0b3a7022c7ebfd9517
ahonson ahonson <ahonson@gmail.com>
2020-12-02 12:49:43 +0000

    Adding api/apikeys.php

---
+++ config/api/apikeys.php
@@ -0,0 +1,7 @@
+<?php
+
+// Your API-key to https://ipstack.com/
+$ipkey = "Your API-key to IPstack";
+
+// Your API-key to https://openweathermap.org/
+$weatherkey = "Your API-key to OpenWeatherMap";
